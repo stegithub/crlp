916d7e9d3b2ae13dcd6d40fd094ff44e7a7c6151
Stefano <stefano.occhi1991@gmail.com>
2018-11-09 00:30:31 +0100

    role, message chat

--- src/views/admin/components/Message.vue
+++ src/views/admin/components/Message.vue
@@ -1,30 +1,46 @@
 <template>
-    <div class="message-container">
+    <div class="message-container" @click="editMessage">
         <div :class="getPosition">
-            {{ message.text }}
+            <span v-if="!editMode">{{ message.sender.firstname + ': ' + message.text }}</span>
+            <input v-if="editMode" @blur="handleBlur" :value="message.text" @input="$emit('messageChanged', {id: message.id, new_value: $event.target.value})" />
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'message',
-        props: {
-            message: {
-                type: Object,
-                required: true
-            }
-        },
-        computed: {
-            getPosition () {
-                if (this.message.sender.user_id === this.$current_user.uid) {
-                    return 'my-message'
-                } else {
-                    return 'message'
-                }
+
+export default {
+    name: 'message',
+    props: {
+        message: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            editMode: false
+        }
+    },
+    computed: {
+        getPosition () {
+            if (this.message.sender.id === this.$current_user.uid) {
+                return 'my-message'
+            } else {
+                return 'message'
             }
         }
+    },
+    methods: {
+        editMessage() {
+            if(this.message.sender.id == this.$current_user.uid)
+                this.editMode = true
+        },
+        handleBlur() {
+            this.editMode = false
+        }
     }
+}
 </script>
 
 <style scoped>
@@ -32,6 +48,7 @@
         text-align: right;
     }
     .message-container {
+        min-height: 20px;
         border: 1px solid silver;
     }
 </style>
--- src/views/admin/pages/Chat.vue
+++ src/views/admin/pages/Chat.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="chat">
-        Artist: {{ this.artist.name }}
+        {{$t("message.artist")}}: {{ this.artist.name }}
         <hr>
-        <message v-for="(message, index) in messages" :message="message" :key="index"/>
+        <message v-for="(message, index) in messages" :message="message" :key="index" @messageChanged="messageChanged($event)"/>
         <div class="md-layout md-alignment-bottom-right">
             <md-button @click="addMessage()" class="md-icon-button">
                 <md-icon>send</md-icon>
@@ -23,35 +23,51 @@ export default {
         return {
             messages: this.$route.params.artistRef.collection('messages').orderBy('created'),
             artist: this.$route.params.artistRef,
-            currentUser: usersRef.where('user_id', '==', this.$current_user.uid)
+            currentUser: usersRef.doc(this.$current_user.uid)
         }
     },
 	data: () => ({
         messages: [],
         text: '',
         artist: {},
-        currentUser: {},
-        currentUserRef: {}
+        currentUser: {}
 	}),
 	created() {
 
     },
     mounted() {
-        this.$firestoreRefs.currentUser.get().then(ref => {
-            this.currentUserRef = ref.docs[0].ref
-        })
+        this.$firestoreRefs.currentUser
     },
     methods: {
         addMessage() {
             if(this.text.length > 0) {
                 this.$route.params.artistRef.collection('messages').add({
                     created: firebase.firestore.FieldValue.serverTimestamp(),
-                    sender: this.currentUserRef,
+                    sender: this.$firestoreRefs.currentUser,
                     text: this.text
+                }).catch(e => {
+                    this.notifyVue('bottom','center', e.message, 'danger')                    
                 })
                 this.text = ''
             }
-        }
+        },
+        messageChanged(data) {
+            this.$route.params.artistRef.collection('messages').doc(data.id).update({
+                text: data.new_value,
+                updated: firebase.firestore.FieldValue.serverTimestamp()
+            }).catch(e => {
+                this.notifyVue('bottom','center', e.message, 'danger')                    
+            })
+        },
+        notifyVue(verticalAlign, horizontalAlign, message, type) {
+            this.$notify({
+                message: message,
+                icon: 'notifications',
+                horizontalAlign: horizontalAlign,
+                verticalAlign: verticalAlign,
+                type: type
+            })
+        },
     },
     components: {
         Message: () => import('../components/Message') //.then(({ MdCard }) => MdCard),
